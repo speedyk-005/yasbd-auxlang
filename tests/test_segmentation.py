ceb45b34649413656e35f4fff8d30d1fc20aa2d4
speedyk-005 <116703678+speedyk-005@users.noreply.github.com>
2026-08-26 17:09:33 -0400

    test: update test

--- tests/test_segmentation.py
+++ tests/test_segmentation.py
@@ -1,16 +1,13 @@
 import pytest
 
 from tests import ALL_TEST_DATA
-from yasbd import BoundaryDetector, register_lang_packs
-
-# Register self
-register_lang_packs(["yasbd_auxlang"])
+from yasbd import BoundaryDetector
 
 
 @pytest.mark.parametrize("lang,test_data", ALL_TEST_DATA.items())
 def test_segment_multiple_langs(subtests, lang, test_data):
     """test that each language's test data passes."""
-    seg = BoundaryDetector(lang=lang)
+    seg = BoundaryDetector(lang=lang, external_lang_packs=["yasbd_auxlang"])
     for marked_text in test_data:
         # Extract the expected sentences by splitting on the marker
         expected = [sent.strip() for sent in marked_text.split("|")]
