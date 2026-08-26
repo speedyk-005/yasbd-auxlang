89e5167e9f7a44938ec97b5b20eec2909b8453b5
speedyk-005 <116703678+speedyk-005@users.noreply.github.com>
2026-08-26 16:34:08 -0400

    style: compact sets to 5+ entries per line

--- yasbd_auxlang/ie.py
+++ yasbd_auxlang/ie.py
@@ -5,78 +5,32 @@ class IeRules(Rules):
     """Rules for Interlingue sentence segmentation."""
 
     TITLE_ABBRVS = Rules.TITLE_ABBRVS | {
-        "Sr",
-        "Sra",
-        "Srta",
-        "Dr",
+        "Sr", "Sra", "Srta", "Dr",
     }
 
     REFERENCE_ABBRVS = Rules.REFERENCE_ABBRVS | {
-        "v",
-        "pg",
-        "cf",
-        "sq",
-        "conc",
-        "nró",
+        "v", "pg", "cf", "sq", "conc", "nró",
     }
 
     INLINE_ONLY_ABBRVS = Rules.INLINE_ONLY_ABBRVS | {
-        "resp",
-        "p.ex",
-        "i.e",
-        "cf",
-        "sq",
-        "conc",
+        "resp", "p.ex", "i.e", "cf", "sq", "conc",
     }
 
     SECTION_MARKERS = Rules.SECTION_MARKERS | {
-        "Capitul",
-        "Parte",
-        "Section",
-        "Articul",
-        "Paragraf",
-        "Volúmine",
-        "Libre",
+        "Capitul", "Parte", "Section", "Articul", "Paragraf", "Volúmine", "Libre",
     }
 
     COMMON_SENT_STARTERS = {
         # Pronouns
-        "Yo",
-        "Tu",
-        "Il",
-        "Ella",
-        "It",
-        "Noi",
-        "Vu",
-        "Ili",
-        "To",
-        "Ti",
-        "Tal",
+        "Yo", "Tu", "Il", "Ella", "It", "Noi", "Vu", "Ili", "To", "Ti", "Tal",
+
         # Question Words
-        "Qui",
-        "Quo",
-        "Qual",
-        "Qualmen",
-        "Ubi",
-        "Quande",
+        "Qui", "Quo", "Qual", "Qualmen", "Ubi", "Quande",
+
         # Adverbs and Connectors
-        "Yes",
-        "No",
-        "Ma",
-        "Tamen",
-        "Forsan",
-        "Naturalmen",
-        "Do",
-        "Pos",
-        "Nu",
-        "Alora",
-        "Finalmente",
-        "Prim",
-        "Secundmen",
+        "Yes", "No", "Ma", "Tamen", "Forsan", "Naturalmen", "Do", "Pos", "Nu", "Alora",
+        "Finalmente", "Prim", "Secundmen",
+
         # Others
-        "Li",
-        "Un",
-        "Altri",
-        "Omni",
-        "Chascun",
+        "Li", "Un", "Altri", "Omni", "Chascun",
     }
